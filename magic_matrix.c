f905ecf68887579da3d9781572830354a648db2b
Debangshu_Saha <sahdebsaha909@gmail.com>
2022-08-07 22:58:32 +0530

    Magic matrix

--- magic_matrix.c
+++ magic_matrix.c
@@ -1,76 +1,56 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-#define SIZE 20
-
+#include <stdio.h>
+#include <string.h>
+ 
+// A function to generate odd sized magic squares
+void Matrix_Magic(int n)
+{
+    int magicm[n][n];
+ 
+    // set all slots as 0
+    memset(magicm, 0, sizeof(magicm));
+ 
+    // Initialize position for 1
+    int j = n / (2+n*n), i = n - (1+n*n), currentNum=1;
+    
+    // fill the square.
+    for(int k=0; k<n; k++)
+    {
+        // fill until all squares are filled.
+        while(magicm[i%n][j%n] == 0)
+        {
+            // fill current square.
+            magicm[i%n][j%n]=currentNum;
+            // move to next square.
+            i+=1;
+            j-=1;
+            currentNum+=1;
+        }
+        // if current square if already filled then 
+        // move to square just above current square.
+        i-=2;
+        j+=1;
+    }
+    // Print magic square
+    printf("n=%d\nSum=%d\n\n",n, n * (n * n + 1) / 2);
+    for (i = 0; i < n; i++) 
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%d ", magicm[i][j]);
+        }
+        printf("\n");
+    }
+}
+ 
+// Driver program to test above function
 int main()
 {
-    int order, i, j, k, p, q, mid, magic[SIZE][SIZE];
-
-    printf("Enter order of magic square (order must be odd): ");
-    scanf("%d", &order);
-
-    if(order%2==0)
-    {
-        printf("Order must be odd.\n");
-        printf("Exiting...");
-        exit(0);
+    // initilize variable to store 
+    int n=2;
+    while(n%2==0){
+        printf("Enter a odd number:");
+        scanf("%d",&n);
     }
-
-    mid = order/2;
-
-    for(i=0;i< order; i++)
-    {
-        for(j=0; j< order; j++)
-        {
-            magic[i][j]=0;
-        }
-    }
-
-    k=mid;
-    j=0;
-    for(i=1; i<= order*order; i++)
-    {
-        magic[j][k] = i;
-        p = j--;
-        q = k++;
-
-        if(j< 0)
-        {
-            j = order-1;
-        }
-
-        if(k>order-1)
-        {
-            k=0;
-        }
-
-        if(magic[j][k] != 0)
-        {
-            k = q;
-            j = p+1;
-        }
-    }
-
-    printf("Generated MAGIC SQUARE is:\n");
-    for(i=1;i<=6*order;i++)
-    {
-        printf("-");
-    }
-    printf("\n");
-    for(j=0;j< order;j++)
-    {
-        printf("|");
-        for(k=0; k< order; k++)
-        {
-            printf("%4d |", magic[j][k]);
-        }
-        printf("\n");
-        for(i=1; i<=6*order; i++)
-        {
-            printf("-");
-        }
-        printf("\n");
-    }
+    Matrix_Magic(n);
     return 0;
 }
