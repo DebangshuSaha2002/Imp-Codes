4a569e53f56967731867a9f32e08d95cdcca1b18
Debangshu_Saha <sahdebsaha909@gmail.com>
2022-08-10 22:29:28 +0530

    Polynomial addition

---
+++ polynomial_add.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+    int LA[10],LB[10],LSum[10],i,m,n,cnt=0;
+    for(i=0;i<=9;i++)
+    {
+        LA[i]=0;
+        LB[i]=0;
+    }
+    printf("\nEnter the order of first Polynomial");
+    scanf("%d",&m);
+    printf("\nEnter the Co-efficient");
+    for(i=m;i>=0;i--)
+    {   
+        scanf("%d",&LA[i]);
+    }
+    printf("\nEnter the order of Second Polynomail");
+    scanf("%d",&n);
+    printf("\nEnter the Co-efficient");
+    for(i=n;i>=0;i--)
+    {
+        scanf("%d",&LB[i]);
+    }
+    if(m>=n)
+    {
+        for(i=m;i>=0;i--)
+        {
+            LSum[i]=LA[i]+LB[i];
+            cnt++;
+        }
+    }
+    else
+    {
+        for(i=n;i>=0;i--)
+        {   
+            LSum[i]=LA[i]+LB[i];
+        }
+    }
+    printf("\nThe resultant polynomial after addition is:A:=");
+    for(i=cnt-1;i>0;i--)
+    {
+        printf("%dX^%d+",LSum[i],i);
+    }
+    printf("%d",LSum[i]);
+    return 0;
+}
