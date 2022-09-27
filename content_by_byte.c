7075aaf8018df04c9c0c6f91e63e5c1ba5d19738
Debangshu_Saha <sahdebsaha909@gmail.com>
2022-09-27 23:52:29 +0530

    Update content_by_byte.c
    Modification on the same code

--- content_by_byte.c
+++ content_by_byte.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-    int store[32];
-    int c = 1;    
-    for(int i = 0 ; i<=24 ; i=i+8)
-    {
-        printf("Enter the Elements of B%d : \n",c);
+    int storage[32] = {0};
+    int count = 1;    
+    for(int i = 0 ; i<=24 ; i+=8){
+        printf("Enter the Elements of B%d with spaces : ",count);
         for(int j = i;j<(i+8);j++)
-        {
-            scanf("%d",&store[j]);
-        }
-        c++;
+            scanf("%d",&storage[j]);
+        count++;
     }
-    int s = 0;
+    int sum = 0;
     for(int i = 0;i<32;i++)
-    {
-        s = s + store[i]*pow(2,(31-i));
-    }
-    printf("The Number is : %d",s);
+        sum = sum + storage[i]*pow(2,(31-i));
+
+    printf("The Required Number : %d",sum);
     return 0 ;
 }
